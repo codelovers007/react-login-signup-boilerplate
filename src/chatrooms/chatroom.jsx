27b1727c613388b139cdb17b90810aff61dd5c8b
Niraj <niraj@gmail.com>
2019-07-29 20:17:52 +0530

    Actioncable implemented

--- src/chatrooms/chatroom.jsx
+++ src/chatrooms/chatroom.jsx
@@ -1,13 +1,116 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Button, Comment, Form, Image, List, Segment } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
+import { configVariable } from '../lib/config';
+import axios from 'axios'
 
-class Chatroom extends React.Component{
-  render(){
-    return(
-      <section className="page2">
-      <h5>ghgjhghjghjgjhgjhgjhghjhg</h5>
-      </section>
-    );
+const ActionCable = require('actioncable')
+
+class Chatroom extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [],
+      chatroom_id: props.match.params.chatroomId,
+      chatroom_name: '',
+      message: '',
+      currentUser: {}
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentWillMount() {
+    let user  = JSON.parse(localStorage.getItem('user'))
+    if (user) {
+	    this.setState({currentUser: user})
+	    this.createSocket(user);
+    }
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': configVariable.chatAuthToken
+    }
+    let queryData = `auth[public_key]=${configVariable.projectPublicKey}&auth[secret_key]=${configVariable.projectSecretKey}&id=${this.state.chatroom_id}`
+    axios.get(`${configVariable.apiChatUrl}/messages?${queryData}`, {headers: headers})
+    .then(response => {
+      if (response.status===200){
+        this.setState({messages: response.data.data.messages, message: response.data.message, chatroom_name: response.data.data.chatroom_name})
+
+      }
+    })
+  }
+
+	handleChange(event){
+		console.log(event)
+	}
+
+	handleSubmit(event){
+	  let data = { body: event.target[0].value, chatroom_id: this.state.chatroom_id, user_id: this.state.currentUser.id}
+	  this.chats.speak(data);
+	}
+
+	createSocket(user) {
+  	let cable = ActionCable.createConsumer(`${configVariable.actionCableUrl}?uid=${user.id}&uname=${user.first_name} ${user.last_name}&public_key=${configVariable.projectPublicKey}`)
+	  this.chats = cable.subscriptions.create({
+	    channel: 'ChatroomChannel'
+	  }, {
+	    connected: () => {},
+	    received: (data) => {
+	      console.log(data);
+	    },
+	    speak: function(chatContent) {
+	      return this.perform('speak', {
+	        content: chatContent
+	      });
+	    }
+	  });
+	}
+
+  render() {
+    return (
+      <div className="messenger" id={this.state.chatroom_id}>
+      	<h4>Welcome to Chatroom:  {this.state.chatroom_name}</h4>
+      	<Segment>
+	      	{this.state.message === "not chatted yet" ? <h5>{this.state.message}</h5>
+	      	:
+		        <List celled>
+		          {
+		            this.state.messages.map((message) => {
+		              return (<List.Item key={message.id}>
+		                      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+		                      <List.Content>
+		                        <List.Header>
+		                          {message.body}
+		                        </List.Header>
+		                      </List.Content>
+		                    </List.Item>)
+		            })
+		          }
+		        </List>
+	      	}
+				  <Comment.Group>
+				    <Comment>
+				      <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+				      <Comment.Content>
+				        <Comment.Author as='a'>Steve Jobes</Comment.Author>
+				        <Comment.Metadata>
+				          <div>2 days ago</div>
+				        </Comment.Metadata>
+				        <Comment.Text>Revolutionary!</Comment.Text>
+				        <Segment>
+					        <Form reply  onSubmit={this.handleSubmit} className="new_message">
+					          <Form.TextArea name="body" onChange={this.handleChange} placeholder='Enter your message...'/>
+					          <Button content='Send Message' labelPosition='left' icon='facebook messenger' primary />
+					        </Form>
+				        </Segment>
+				      </Comment.Content>
+				    </Comment>
+				  </Comment.Group>
+			  </Segment>
+      </div>
+    )
   }
 }
 
-export {Chatroom};
+export {Chatroom}
--- src/lib/config.js
+++ src/lib/config.js
@@ -4,5 +4,6 @@ export const configVariable = {
   apiChatUrl: 'http://localhost:4000/api/v1',
   projectPublicKey: 'fedb4c7cd79a5e29e765',
 	projectSecretKey: '3aa3573254d8fd184847',
-	chatAuthToken: 'ZWNoYXRjbGllbnQ6Y2xpZW50JHNlY3JldA=='
+	chatAuthToken: 'ZWNoYXRjbGllbnQ6Y2xpZW50JHNlY3JldA==',
+	actionCableUrl: 'ws://localhost:4000/cable'
 }
